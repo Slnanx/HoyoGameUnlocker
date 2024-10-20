355640db3e17060222e18261fb3195ad91e97024
Slnanx <SudakaLemonTea@outlook.com>
2024-10-20 18:53:38 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/HoyoGameUnlocker.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Privia/Desktop/Coding/Project/HoyoGameUnlocker/src/ConfigurationIO.cpp" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/ConfigurationIO.cpp.obj" "gcc" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/ConfigurationIO.cpp.obj.d"
+  "C:/Users/Privia/Desktop/Coding/Project/HoyoGameUnlocker/src/ConsoleShow.cpp" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/ConsoleShow.cpp.obj" "gcc" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/ConsoleShow.cpp.obj.d"
+  "C:/Users/Privia/Desktop/Coding/Project/HoyoGameUnlocker/src/GameInfo.cpp" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/GameInfo.cpp.obj" "gcc" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/GameInfo.cpp.obj.d"
+  "C:/Users/Privia/Desktop/Coding/Project/HoyoGameUnlocker/src/GetAdministratorPrivileges.cpp" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/GetAdministratorPrivileges.cpp.obj" "gcc" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/GetAdministratorPrivileges.cpp.obj.d"
+  "C:/Users/Privia/Desktop/Coding/Project/HoyoGameUnlocker/src/GetGamePath.cpp" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/GetGamePath.cpp.obj" "gcc" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/GetGamePath.cpp.obj.d"
+  "C:/Users/Privia/Desktop/Coding/Project/HoyoGameUnlocker/src/main.cpp" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/main.cpp.obj" "gcc" "CMakeFiles/HoyoGameUnlocker.dir/HoyoGameUnlocker/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
