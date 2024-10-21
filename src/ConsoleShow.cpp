5e3aad69dc7c356543099684c5c9f03a694f087c
Slnanx <SudakaLemonTea@outlook.com>
2024-10-21 18:07:54 +0800

    Optimize the code directory structure.

--- src/ConsoleShow.cpp
+++ src/ConsoleShow.cpp
@@ -1,4 +1,4 @@
-#include "..\include\ConsoleShow.h"
+#include "../include/ConsoleShow.h"
 
 // 确保STD_OUTPUT_HANDLE已定义
 #ifndef STD_OUTPUT_HANDLE
@@ -8,13 +8,14 @@
 // 使鼠标滚轮滚动不会导致文本的滚动
 void DisableConsoleScroll()
 {
+    // ReSharper disable once CppLocalVariableMayBeConst
     HANDLE hand = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO info;
 
     if (GetConsoleScreenBufferInfo(hand, &info))
     {
         // 直接在设置缓冲区大小时进行类型转换
-        COORD size = {static_cast<SHORT>(info.srWindow.Right + 1), static_cast<SHORT>(info.srWindow.Bottom + 1)};
+        const COORD size = {static_cast<SHORT>(info.srWindow.Right + 1), static_cast<SHORT>(info.srWindow.Bottom + 1)};
         SetConsoleScreenBufferSize(hand, size);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
-#include "..\include\SimpleIni.h"
-#include "..\include\GetGamePath.h"
-#include "..\include\ConfigurationIO.h"
-#include "..\include\ConsoleShow.h"
-#include "..\include\GetAdministratorPrivileges.h"
+#include "../include/SimpleIni.h"
+#include "../include/ConfigurationIO.h"
+#include "../include/ConsoleShow.h"
+#include "../include/GetAdministratorPrivileges.h"
 
 int main()
 {
     SetConsoleTitleW(L"HoyoFPSUnlocker");
-
     GetAdministratorPrivilege(); // 尝试自动获取管理员权限，效果就是程序打开的时候会闪一下
     DisableConsoleScroll();      // 禁止控制台滚动
-    std::filesystem::path GamePath = LoadConfiguration()->string();
+    const std::filesystem::path GamePath = LoadConfiguration()->string();
 
-    MessageBoxW(NULL, GamePath.wstring().c_str(), L"文件路径", MB_OK);
+    MessageBoxW(nullptr,GamePath.c_str(),L"HoyoFPSUnlocker",MB_OK);
 }
